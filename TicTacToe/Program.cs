9b34130f6251c91625eaf25f767e6bcac32558ae
zver-id <helix999@mail.ru>
2025-05-01 14:30:23 +0400

    Добавил отрисовку доски в консоли

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,6 +6,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        var game = new Board();
+        game.DrawBoard();
     }
+}
+
+class Board
+{
+    char[,] board = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+
+    public void DrawBoard()
+    {
+        Console.WriteLine("_____________");
+        for (var i = 0; i < 3; i++)
+        {
+            Console.Write("| ");
+            for (var j = 0; j < 3; j++)
+            {
+                Console.Write(board[i, j]);
+                Console.Write(" | ");
+            }
+            Console.Write("\n\r");
+            Console.WriteLine("_____________");
+        }
+    }
+    
 }
